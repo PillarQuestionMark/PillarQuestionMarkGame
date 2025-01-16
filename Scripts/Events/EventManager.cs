fcdda2e57cde77ebf418768ffca5d667fa5ff571
samjsea <samuelcolston@gmail.com>
2025-01-16 13:54:15 -0500

    Added EventManager Singleton and Handling for Audio Effects
    I made an event manager singleton and added it to the autoload settings so it should be globally accessible on ready. I also added a scene to add audio players and a class to access them and attach them to events.

---
+++ Scripts/Events/EventManager.cs
@@ -0,0 +1,45 @@
+﻿using Godot;
+using System;
+using System.Collections.Generic;
+
+public partial class EventManager : Node
+{
+    public delegate void EventAction(Dictionary<string, object> eventData);
+    private Dictionary<string, EventAction> eventListeners = new Dictionary<string, EventAction>();
+
+    public static EventManager Instance { get; private set; }
+
+    public override void _Ready()
+    {
+        Instance = this;
+    }
+
+    public void Subscribe(string eventType, EventAction listener)
+    {
+        if (!eventListeners.ContainsKey(eventType))
+        {
+            eventListeners[eventType] = listener;
+        }
+        else
+        {
+            eventListeners[eventType] += listener;
+        }
+    }
+
+    public void Unsubscribe(string eventType, EventAction listener)
+    {
+        if (eventListeners.ContainsKey(eventType))
+        {
+            eventListeners[eventType] -= listener;
+        }
+    }
+
+    public void Dispatch(string eventType, Dictionary<string, object> eventData)
+    {
+        if (eventListeners.ContainsKey(eventType))
+        {
+            eventListeners[eventType].Invoke(eventData);
+        }
+    }
+}
+        
--- Scripts/PlayerBehavior/States/SprintingState.cs
+++ Scripts/PlayerBehavior/States/SprintingState.cs
@@ -9,13 +9,11 @@ public class SprintingState : WalkingState
 
     public override void Enter()
     {
-        GD.Print("Entering Sprinting State");
         Speed *= SprintMultiplier;
         base.Enter();
     }
     public override void Exit()
     {
-        GD.Print("Exiting Sprinting State");
         Speed /= SprintMultiplier;
         base.Exit();
     }
