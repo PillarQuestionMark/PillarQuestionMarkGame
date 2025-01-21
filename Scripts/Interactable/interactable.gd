81005ddf41f1c183964704185b729f59a549a400
samjsea <samuelcolston@gmail.com>
2025-01-20 20:26:01 -0500

    Add Interactable Progress Bar
    I added a UI element to display the progress of completing an interaction.

--- Scripts/Interactable/interactable.gd
+++ Scripts/Interactable/interactable.gd
@@ -3,17 +3,22 @@ extends Node
 @export var interact_duration = 2.0
 @export var interact_action = "interact"
 @export var interact_id = ""
-@export var max_interaction_distance = 5.0  # Maximum distance to interact
+@export var max_interaction_distance = 5.0
 
 var timer = null
 var is_holding = false
 var interacting_node = null
 
-@onready var player_node = get_tree().get_root().get_node("Playground/Player")  # Replace with the correct path
+@onready var player_node = get_tree().get_root().get_node("Playground/Player")
+@export var progress_bar_scene: PackedScene = preload("res://Scripts/Interactable/interactable_bar.tscn")
+var progress_bar: Node3D = null
 
 signal interaction_triggered(node, interact_id)
 
 func _ready():
+	if progress_bar_scene:
+		progress_bar = progress_bar_scene.instantiate()
+		add_child(progress_bar)
 	timer = Timer.new()
 	timer.wait_time = interact_duration
 	timer.connect("timeout", Callable(self, "_on_timer_timeout"))
@@ -36,20 +41,40 @@ func _on_timer_timeout():
 		emit_signal("interaction_triggered", self, interact_id)
 		timer.stop()
 
+func _process(delta):
+	var closest_interactable = _find_closest_interactable()
+	if not closest_interactable:
+		progress_bar.set("visible", false)
+		return	
+	progress_bar.set("visible", true) 
+	var background = progress_bar.get_node("Background") as MeshInstance3D
+	var foregroundf = progress_bar.get_node("ForegroundFront") as MeshInstance3D
+	var foregroundb = progress_bar.get_node("ForegroundBack") as MeshInstance3D
+	background.scale.x = 1.0
+	if is_holding and timer:
+		var progress_ratio = (interact_duration - timer.get_time_left()) / interact_duration
+		foregroundf.scale.x = progress_ratio
+		foregroundb.scale.x = progress_ratio
+	else:
+		foregroundf.scale.x = 0
+		foregroundb.scale.x = 0
+	progress_bar.global_transform = closest_interactable.global_transform.translated(Vector3(0, 1, 0))
+	var direction_to_player = player_node.global_position - progress_bar.global_position
+	var angle = direction_to_player.angle_to(Vector3.FORWARD)
+	var current_rotation = progress_bar.rotation_degrees
+	current_rotation.y = angle * (180 / PI)
+	progress_bar.rotation_degrees = current_rotation
+
 func _find_closest_interactable() -> Node:
 	if not player_node:
 		return null
-
 	var closest_node = null
 	var closest_distance = max_interaction_distance
-	var player_position = player_node.global_position  # Use global_transform.origin for 3D
-
+	var player_position = player_node.global_position
 	for interactable in get_tree().get_nodes_in_group("interactables"):
-		var interactable_position = interactable.global_position  # Use global_transform.origin for 3D
+		var interactable_position = interactable.global_position
 		var distance = player_position.distance_to(interactable_position)
-
 		if distance <= closest_distance:
 			closest_distance = distance
 			closest_node = interactable
-
 	return closest_node
