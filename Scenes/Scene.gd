bef118bb20d4a18fe3f02a3d529deb4d248830c8
SincerelySeven <115359401+SincerelySeven@users.noreply.github.com>
2025-01-26 17:38:43 -0500

    Moved load code to new function
    Moved the player information loading code from the _ready() function to a new _load_data() function. This is to have less clutter in the _ready() function in case we need to use it for more functionality.

--- Scenes/Scene.gd
+++ Scenes/Scene.gd
@@ -2,8 +2,15 @@ extends Node3D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	## loads the player position from the save data dictionary
-	
+	_load_data()
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+## loads the player information from the save data dictionary
+func _load_data() -> void:
 	var position_array = PlayerData.data["player_position"]
 	$Player.position = Vector3(position_array[0], position_array[1], position_array[2])
 	
@@ -12,8 +19,3 @@ func _ready() -> void:
 	
 	var camera_array = PlayerData.data["camera_rotation"]
 	$Player.Camera.rotation =  Vector3(camera_array[0], camera_array[1], camera_array[2])
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
