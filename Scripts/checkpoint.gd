551a38b5fbbed22251a5ca4b0114392903c5cd34
SincerelySeven <115359401+SincerelySeven@users.noreply.github.com>
2025-01-31 00:22:42 -0500

    Added checkpoint areas
    Added the ability to get near a checkpoint and have it update your position. Mainly for testing right now, though I am completely fine with having checkpoints work this way as well. Perhaps we should also add a visual if we keep it.

--- Scripts/checkpoint.gd
+++ Scripts/checkpoint.gd
@@ -9,3 +9,11 @@ func set_player(player : CharacterBody3D) -> void:
 	player.get_pivot().rotation = rotation
 	##player.Camera.rotation = Vector3(deg_to_rad(camera_rotation.x), deg_to_rad(camera_rotation.y), deg_to_rad(camera_rotation.z))
 	player.Camera.rotation = rotation
+
+
+## called with checkpoints having an area3d
+## node structure as follows:
+## Checkpoint <- Area3D (link body_entered here) <- CollisionShape3D 
+func _on_area_3d_body_entered(_body: Node3D) -> void:
+	PlayerData.data["checkpoint"] = id
+	print("CHECKPOINT: " + String.num_int64(id))
