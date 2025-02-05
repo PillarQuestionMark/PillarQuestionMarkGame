024febc1b75a83b82e4bd9e2a4bc88ef30980deb
dgoorskey <dersu.goorskey@gmail.com>
2025-02-05 11:40:01 -0500

    add configuration warning to enforce checkpoint id uniqueness

--- Components/checkpoint.gd
+++ Components/checkpoint.gd
@@ -1,8 +1,12 @@
+@tool
 extends Area3D
 class_name Checkpoint
 
 ## id should be unique across a level
-@export var id : int = 0
+@export var id : int = 0:
+	set(value):
+		id = value
+		update_configuration_warnings()
 
 
 func set_player(player : CharacterBody3D) -> void:
@@ -19,3 +23,14 @@ func _on_body_entered(body: Node3D) -> void:
 	PlayerData.data["checkpoint"] = id
 	Logger.debug("checkpoint %d: saving player data" % id)
 	PlayerData.save_data()
+
+
+func _get_configuration_warnings() -> PackedStringArray:
+	var warnings: Array[String] = []
+	
+	for c in get_tree().get_nodes_in_group("checkpoint"):
+		var c_id := (c as Checkpoint).id
+		if c_id == id and c != self:
+			warnings.append("Duplicate checkpoint id with checkpoint \"%s\". Checkpoint id must be unique across the current scene." % c.name)
+	
+	return warnings
