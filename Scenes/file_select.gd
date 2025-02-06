8f662bbe0d43de9902b9f851093154d06e241b8c
SincerelySeven <115359401+SincerelySeven@users.noreply.github.com>
2025-02-05 21:32:46 -0500

    Added documentation formatting to file select
    The class is now called FileSelect, and has better documentation to it.

--- Scenes/file_select.gd
+++ Scenes/file_select.gd
@@ -1,9 +1,9 @@
-extends Node
+class_name FileSelect extends Node
 ## The file select screen, showcasing the player's save files.
 
-var FileContainer
+var FileContainer ## Reference to a node to base UI elements off of.
 
-# Called when the node enters the scene tree for the first time.
+## Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	FileContainer = $CanvasLayer/PanelContainer/MarginContainer/VBoxContainer
 	FileContainer.get_node("File1").grab_focus()
@@ -11,7 +11,7 @@ func _ready() -> void:
 	for file in [1, 2, 3]:
 		_read_file(FileUtility.save_file_name + String.num_int64(file) + FileUtility.save_file_ending, file)
 	
-## used to read the save files for menu information
+## Reads the given file number and writes the important data onto the given save file slot.
 func _read_file(filePath : String, number : int):
 	var data = FileUtility.read_file(filePath)
 	if (data != null):
@@ -21,12 +21,12 @@ func _read_file(filePath : String, number : int):
 		FileContainer.get_node("File" + String.num_int64(number) + "/Time").text = "Empty"
 		FileContainer.get_node("File" + String.num_int64(number) + "/Flames").text = ""
 
-## used when a file is pressed. passed an int representing which save file to use
+## Called when a file is pressed. The function is passed an int representing which save file to play.
 func _on_file_pressed(file : int) -> void:
 	PlayerData.set_file_number(file)
 	PlayerData.load_data()
 	
-## used when a file is being deleted. passed an int representing which save file to use
+## Called when a file is being deleted. The function is passed an int representing which save file to delete.
 func _on_file_delete(file : int) -> void:
 	var deleting = FileUtility.save_file_name + String.num_int64(file) + FileUtility.save_file_ending
 	FileUtility.delete_file(deleting)
