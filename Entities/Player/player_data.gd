9806b752b070260497c2e2aa1e249e1976756577
SincerelySeven <115359401+SincerelySeven@users.noreply.github.com>
2025-02-05 21:09:19 -0500

    Added FileUtility class
    This class allows PlayerData and file_select to ignore certain details about loading and writing to files. This will also abstract the manner of interacting with files, which will be helpful for anything that needs to read or write files in the future, since the code would be duplicated anyways.

--- Entities/Player/player_data.gd
+++ Entities/Player/player_data.gd
@@ -4,8 +4,6 @@ extends Node
 ## based on documentation from Godot on JSON and FileAccess
 
 var _file
-var _file_name = "user://save"
-var _file_ending = ".pillar"
 
 var start_time
 
@@ -27,50 +25,28 @@ var data = {
 
 ## reads the save file values into the data dictionary
 func load_data() -> void:
-	##_write_default() ## used for writing the default file (DO NOT UNCOMMENT UNLESS YOU KNOW WHAT THIS DOES)
-	var save
-	var json = JSON.new() ## created for better error messages
+	## used for writing the default file (DO NOT UNCOMMENT UNLESS YOU KNOW WHAT THIS DOES)
+	##FileUtility.write_file("res://default_save.pillar", data)
+
 	if (FileAccess.file_exists(_file)):
-		var file = FileAccess.open(_file, FileAccess.READ) ## open the save file
-		save = file.get_as_text() ## get the dictionary from the save file
-		file.close()
+		data = FileUtility.read_file(_file)
 	else:
-		var default_file = FileAccess.open("res://default_save.pillar", FileAccess.READ)
-		var file = FileAccess.open(_file, FileAccess.WRITE_READ) ## open the save file
-		save = default_file.get_as_text() ## get the default data
-		file.store_line(save) ## save the default data to the new file
-		default_file.close()
-		file.close()
-	
-	var error = json.parse(save)
-	if error == OK:
-		data = json.data
-	else:
-		Logger.error("playerdata: json error at line %s: %s" % [json.get_error_line(), json.get_error_message()])
+		data = FileUtility.read_file("res://default_save.pillar")
+		FileUtility.write_file(_file, data)
 	
 	get_tree().change_scene_to_file(data["current_scene"]) ## load the scene
 	
 	start_time = Time.get_unix_time_from_system()
 
-## writes the current data dictionary into the save file
+## Writes the current data dictionary into the save file.
 func save_data() -> void:
-	## THIS BELOW MAY NOT BE NEEDED ANYMORE
-	## tell the player, and any other saving objects, to save their data
-	var save_objects = get_tree().get_nodes_in_group("save")
-	for saving in save_objects:
-		saving.save()
-		
 	## get playtime
 	data["playtime"] += Time.get_unix_time_from_system() - start_time
-	
-	var save = JSON.stringify(data, "\t") ## turn the data into JSON
-	var file = FileAccess.open(_file, FileAccess.WRITE) ## open the save file, should overwrite
-	file.store_line(save) ## write the JSON data
-	file.close() ## close the file
+	FileUtility.write_file(_file, data)
 		
 ## used when loading the game to signify which file we are playing on
 func set_file_number(file : int) -> void:
-	_file = _file_name + String.num_int64(file) + _file_ending
+	_file = FileUtility.save_file_name + String.num_int64(file) + FileUtility.save_file_ending
 	
 ## called when a flame is collected
 func flame_collected(flame_id: int, island_id: int):
@@ -83,16 +59,4 @@ func get_island_flames(island_id: int) -> Array:
 	## if the flames dictionary has the island already, just return that list
 	## else, create the island entry with an empty list
 	return data["collected_flames"].get_or_add(String.num_int64(island_id), [])
-	
-	
-## dev function used to write the default save file when changes are made
-func _write_default() -> void:
-	## open the default save to write
-	var default_file = FileAccess.open("res://default_save.pillar", FileAccess.WRITE)
-	## get the default data seen above
-	var save = JSON.stringify(data, "\t")
-	## write the default data to the save
-	default_file.store_line(save)
-	## close the default file
-	default_file.close()
 	
--- Scenes/file_select.gd
+++ Scenes/file_select.gd
@@ -1,7 +1,5 @@
 extends Node
-
-var _file_name = "user://save"
-var _file_ending = ".pillar"
+## The file select screen, showcasing the player's save files.
 
 var FileContainer
 
@@ -11,56 +9,17 @@ func _ready() -> void:
 	FileContainer.get_node("File1").grab_focus()
 	
 	for file in [1, 2, 3]:
-		_read_file(_file_name + String.num_int64(file) + _file_ending, file)
+		_read_file(FileUtility.save_file_name + String.num_int64(file) + FileUtility.save_file_ending, file)
 	
 ## used to read the save files for menu information
 func _read_file(filePath : String, number : int):
-	var save
-	var json = JSON.new() ## created for better error messages
-	if (FileAccess.file_exists(filePath)):
-		var file = FileAccess.open(filePath, FileAccess.READ) ## open the save file
-		save = file.get_as_text() ## get the dictionary from the save file
-		file.close()
-	else:
-		FileContainer.get_node("File" + String.num_int64(number) + "/Time").text = "Empty"
-		FileContainer.get_node("File" + String.num_int64(number) + "/Flames").text = ""
-		return
-		
-	var error = json.parse(save)
-	if error == OK:
-		var data = json.data
-		FileContainer.get_node("File" + String.num_int64(number) + "/Time").text = "Time: " + _convert_time(data["playtime"])
+	var data = FileUtility.read_file(filePath)
+	if (data != null):
+		FileContainer.get_node("File" + String.num_int64(number) + "/Time").text = "Time: " + FileUtility.get_time_string(data["playtime"])
 		FileContainer.get_node("File" + String.num_int64(number) + "/Flames").text = "Flames: " + String.num(data["total_flames"])
 	else:
 		FileContainer.get_node("File" + String.num_int64(number) + "/Time").text = "Empty"
 		FileContainer.get_node("File" + String.num_int64(number) + "/Flames").text = ""
-		print("ERROR: ", json.get_error_message(), " at line ", json.get_error_line())
-
-
-## converts the playtime in seconds to a more viewable format
-func _convert_time(time : int) -> String:
-	var seconds
-	var minutes
-	var hours
-	
-	## get the seconds
-	seconds = String.num_int64(time % 60).pad_zeros(2)
-	time /= 60 ## move to minutes
-	
-	## get the minutes, if any
-	minutes = String.num_int64(time % 60).pad_zeros(2)
-	time /= 60 ## move to hours
-	
-	if (time > 0):
-		hours = String.num_int64(time)
-	else:
-		hours = "0"
-	
-	return hours + ":" + minutes + ":" + seconds
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
 
 ## used when a file is pressed. passed an int representing which save file to use
 func _on_file_pressed(file : int) -> void:
@@ -69,9 +28,6 @@ func _on_file_pressed(file : int) -> void:
 	
 ## used when a file is being deleted. passed an int representing which save file to use
 func _on_file_delete(file : int) -> void:
-	var deleting = _file_name + String.num_int64(file) + _file_ending
-	## this should just move the deleted safe file to the trash
-	if (FileAccess.file_exists(deleting)):
-		OS.move_to_trash(ProjectSettings.globalize_path(deleting)) 
-	## DirAccess.remove_absolute(deleting) this will PERMANENTLY delete the file
-	_read_file(_file_name + String.num_int64(file) + _file_ending, file)
+	var deleting = FileUtility.save_file_name + String.num_int64(file) + FileUtility.save_file_ending
+	FileUtility.delete_file(deleting)
+	_read_file(deleting, file)
