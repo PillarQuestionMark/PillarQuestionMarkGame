47b8b3543d3f0468bd75cbf9bdb667a096e5dee5
SincerelySeven <115359401+SincerelySeven@users.noreply.github.com>
2025-02-05 21:26:36 -0500

    Adjusted player_data class
    Added two new methods for future use.
    
    Adjusted the load_scene method to make it more widely useable.
    
    Fixed some documentation issues.

--- Entities/Player/player_data.gd
+++ Entities/Player/player_data.gd
@@ -1,13 +1,13 @@
 extends Node
-## stores the player's save data, which is accessed in many places between many scenes
+## Stores the player's save data, which is accessed in many places between many scenes.
 
-## based on documentation from Godot on JSON and FileAccess
+# based on documentation from Godot on JSON and FileAccess
 
 var _file
 
-var start_time
+var start_time ## The time at which the save file is opened, or last saved. Used for playtime calculations.
 
-## dictionary of data (default values to be overwritten)
+## Dictionary of player data (default values to be overwritten).
 var data = {
 	"playtime" = 0.0, ## playtime for the save file
 	"total_flames" = 0,
@@ -23,7 +23,7 @@ var data = {
 	"wall_slide_unlocked" = false
 }
 
-## reads the save file values into the data dictionary
+## Reads the save file values into the data dictionary.
 func load_data() -> void:
 	## used for writing the default file (DO NOT UNCOMMENT UNLESS YOU KNOW WHAT THIS DOES)
 	##FileUtility.write_file("res://default_save.pillar", data)
@@ -44,22 +44,48 @@ func save_data() -> void:
 	data["playtime"] += Time.get_unix_time_from_system() - start_time
 	FileUtility.write_file(_file, data)
 		
-## used when loading the game to signify which file we are playing on
+## Used when loading the game to signify which file we are playing on
 func set_file_number(file : int) -> void:
 	_file = FileUtility.save_file_name + String.num_int64(file) + FileUtility.save_file_ending
 	
-## called when a flame is collected
+## Updates the player's save data to include the given flame.
 func flame_collected(flame_id: int, island_id: int):
 	## get list of flames and add the newly collected flame
 	get_island_flames(island_id).append(flame_id)
 	data["total_flames"] += 1
 	
-## used to get the list of flames on an island
+## Returns the list of flames collected on an island - based on island id.
 func get_island_flames(island_id: int) -> Array:
 	## if the flames dictionary has the island already, just return that list
 	## else, create the island entry with an empty list
 	return data["collected_flames"].get_or_add(String.num_int64(island_id), [])
 	
-## called when you die to reload scene, as well as travelling between scenes
-func load_scene() -> void:
-	get_tree().change_scene_to_file(data["current_scene"]) ## load the scene
+## Loads the player into the given scene at the given checkpoint.
+## If no scene and checkpoint passed, it will default to the saved data.
+## If scene and checkpoint passed, it will update the save file.
+func load_scene(next_scene : String = data["current_scene"], checkpoint : int = data["checkpoint"]) -> void:
+	data["current_scene"] = next_scene
+	data["checkpoint"] = checkpoint
+	get_tree().change_scene_to_file(next_scene) ## load the scene
+	
+## Sets the checkpoint to spawn at in player data. 
+## Separate from [method load_scene] for collecting checkpoints without loading a scene.
+## If you also want to load a scene, use [method load_scene].
+func update_checkpoint(checkpoint : int):
+	data["checkpoint"] = checkpoint
+
+## Unlocks the given ability in the player save. The string must match exactly the data string. [br]
+## Normal Jump = "jump" [br]
+## Double Jump = "double_jump" [br]
+## Dash = "dash" [br]
+## Slam = "slam" [br]
+## Wall Slide = "wall_slide"
+func unlock_move(move : String) -> void:
+	match move:
+		"jump": ## error checking to avoid losing double jump
+			if (data["max_jumps"] < 1):
+				data["max_jumps"] = 1
+		"double_jump": ## no error checking needed.
+			data["max_jumps"] = 2
+		_: ## default case, not handled differently
+			data[move + "_unlocked"] = true
