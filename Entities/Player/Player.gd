b2dca587c2d120cfb3c09a8efb01aeae0a816197
SincerelySeven <115359401+SincerelySeven@users.noreply.github.com>
2025-02-15 16:33:06 -0500

    Added jump and slam functions to mushrooms.
    Holding the jump button when landing on a mushroom will bounce the player higher.
    
    Slamming onto a mushroom will bounce the player even higher.
    
    You can toggle mushrooms to be slam only, meaning they will only bounce the player when you slam.

--- Entities/Player/Player.gd
+++ Entities/Player/Player.gd
@@ -27,6 +27,8 @@ var jumps_left : int = 0 # how many jumps left
 
 var slamjump_unlocked : bool = true
 
+@onready var state_machine : StateMachine = $StateMachine
+
 @onready var jump_sound: AudioStreamPlayer = %AudioStreamPlayer
 @onready var wall_slide_particles: GPUParticles3D = %WallSlideParticles
 @onready var mesh : MeshInstance3D = $Pivot/MeshInstance3D
--- Obstacles/bouncy_mushroom.gd
+++ Obstacles/bouncy_mushroom.gd
@@ -1,6 +1,9 @@
 class_name BouncyMushroom extends Node3D
 
-@export var bounce_height : float = 20
+@export var bounce_height : float = 10
+@export var jump_height : float = 20
+@export var slam_height : float = 40
+@export var slam_only : bool = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -14,6 +17,23 @@ func _process(delta: float) -> void:
 
 ## Called when the player lands on the mushroom.
 func _on_area_3d_body_entered(body: Node3D) -> void:
-	print("om")
-	if (body is Player && body.is_on_floor()):
-		body.velocity.y += bounce_height
+	if !(body is Player) or !body.is_on_floor(): ## avoid checks on non-players
+		return
+	## is check on ground needed?
+	if !slam_only or _check_slam(body): ## if slam only check slam, else do nothing
+		body.touched_ground() ## reset jumps (for double jumping)
+		body.velocity.y += _check_height(body) ## give bounce impulse
+		body.move_and_slide() ## this is needed to avoid being able to normal jump after bounce
+		body.jumps_left -= 1 ## bounce counts as an initial jump
+		
+func _check_height(player : Player) -> float:
+	var height = bounce_height ## default to normal bounce height
+	if _check_slam(player):
+		height = slam_height
+	elif (Input.is_action_pressed("jump")):
+		height = jump_height
+	
+	return height
+	
+func _check_slam(player : Player) -> bool:
+	return player.state_machine.state.name == "Slamming" or player.can_slamjump()
