43a9e065a1bd2fd166dc45e7a97efe18a0bbe549
dgoorskey <dersu.goorskey@gmail.com>
2025-02-22 23:58:18 -0500

    use separate materials for on and off lanterns

--- Scenes/props/lantern/lantern.gd
+++ Scenes/props/lantern/lantern.gd
@@ -13,9 +13,11 @@ extends MeshInstance3D
 		on = value
 		if _is_ready: _update_on()
 
-@export var material: StandardMaterial3D = preload("res://Scenes/props/lantern/lantern_material_default.tres"):
+const default_material = preload("res://Scenes/props/lantern/lantern_material_default.tres")
+@export var material: StandardMaterial3D = default_material:
 	set(value):
 		material = value
+		update_configuration_warnings()
 		if _is_ready:
 			_update_material()
 			_update_on() # update emission_enabled
@@ -29,8 +31,8 @@ var _is_ready := false # prevent setters from accessing child nodes before _read
 
 func _ready() -> void:
 	_is_ready = true
-	_update_on()
 	_update_material()
+	_update_on()
 
 #func _update_color() -> void:
 	#material.albedo_color = color
@@ -38,7 +40,7 @@ func _ready() -> void:
 	#_light.light_color = color
 
 func _update_on() -> void:
-	material.emission_enabled = on
+	#material.emission_enabled = on # causes issues with shared materials. instead, use separate materials for on/off lanterns.
 	_light.visible = on
 	if on:
 		_light.light_bake_mode = Light3D.BAKE_STATIC
@@ -50,3 +52,8 @@ func _update_material() -> void:
 	
 	# update light color based on material
 	_light.light_color = material.albedo_color
+
+func _get_configuration_warnings() -> PackedStringArray:
+	if material == default_material:
+		return ["Set the lantern material to something other than the default, e.g. lantern_material_off_c33c40.tres."]
+	return []
