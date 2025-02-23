d2b169814b3c0871318e267d11f17bcd644a34dd
dgoorskey <dersu.goorskey@gmail.com>
2025-02-22 23:42:43 -0500

    make lanterns use shared materials instead of constantly duplicating their own

--- Scenes/props/lantern/lantern.gd
+++ Scenes/props/lantern/lantern.gd
@@ -11,50 +11,42 @@ extends MeshInstance3D
 @export var on := false:
 	set(value):
 		on = value
-		if _is_ready:
-			_duplicate_if_needed()
-			_update_on()
+		if _is_ready: _update_on()
 
-@export var color := Color("c33c40"):
+@export var material: StandardMaterial3D = preload("res://Scenes/props/lantern/lantern_material_default.tres"):
 	set(value):
-		color = value
-		if _is_ready: # _update_color() will fail if _ready() isn't done yet
-			_duplicate_if_needed()
-			_update_color()
+		material = value
+		if _is_ready:
+			_update_material()
+			_update_on() # update emission_enabled
 
 @export_group("Internal")
-@export var surface_material_override_idx := 0 ## index of surface material override to use as the colored lantern material
+@export var _surface_material_override_idx := 0 ## index of surface material override to use as the colored lantern material
 
-@onready var light: OmniLight3D = $OmniLight3D
-@onready var material: StandardMaterial3D = preload("res://Scenes/props/lantern/lantern_material.tres")
+@onready var _light: OmniLight3D = $OmniLight3D
 
-var _is_ready := false
+var _is_ready := false # prevent setters from accessing child nodes before _ready() is done
 
 func _ready() -> void:
-	_duplicate_if_needed()
-	
 	_is_ready = true
 	_update_on()
-	_update_color()
+	_update_material()
 
-func _duplicate_if_needed() -> void:
-	# only duplicate if absolutely necessary, otherwise resource ids will change way too much and make git merges a pain
-	if material.albedo_color == color and material.emission == color:
-		return
-	
-	material = material.duplicate()
-	set_surface_override_material(surface_material_override_idx, material)
-
-func _update_color() -> void:
-	material.albedo_color = color
-	#material.albedo_color.a = 0.9 # transparency looks kinda ugly rn
-	material.emission = color
-	light.light_color = color
+#func _update_color() -> void:
+	#material.albedo_color = color
+	#material.emission = color
+	#_light.light_color = color
 
 func _update_on() -> void:
 	material.emission_enabled = on
-	light.visible = on
+	_light.visible = on
 	if on:
-		light.light_bake_mode = Light3D.BAKE_STATIC
+		_light.light_bake_mode = Light3D.BAKE_STATIC
 	else:
-		light.light_bake_mode = Light3D.BAKE_DISABLED
+		_light.light_bake_mode = Light3D.BAKE_DISABLED
+
+func _update_material() -> void:
+	set_surface_override_material(_surface_material_override_idx, material)
+	
+	# update light color based on material
+	_light.light_color = material.albedo_color
