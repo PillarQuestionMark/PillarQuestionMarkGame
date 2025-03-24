f0173d1a236a2bceaeb3047fddf86e0e5d9e847d
Tsanzevino <zeldafanta@gmail.com>
2025-03-24 04:02:07 -0400

    Added ray detection for grapple point occlusion

--- Entities/Player/range_finder.gd
+++ Entities/Player/range_finder.gd
@@ -6,19 +6,12 @@ var cam : SpringArm3D
 func _ready():
 	cam = owner.Camera
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	rotation = cam.rotation
-
 func _on_area_entered(area):
 	#Toggle color/indicator for grappleable objects
 	if area is not GrappleablePoint:
 		return
 	area.toggle()
 	targets.append(area)
-	print("TARGETS: ")
-	for target in targets:
-		print(target.name)
 
 func _on_area_exited(area):
 	#Toggle color/indicator for grappleable objects
@@ -26,25 +19,31 @@ func _on_area_exited(area):
 		return
 	area.toggle()
 	targets.remove_at(targets.find(area))
-	print("TARGETS: ")
-	for target in targets:
-		print(target.name)
 
 func get_best_target() -> GrappleablePoint:
-	var maxDot : float = 0.0
+	var maxDot : float = 0.9
 	var bestTarget = null
 	for target : GrappleablePoint in targets:
 		# Creating Variables for readability.
 		var targetPos : Vector3 = target.global_position
-		var camForward : Vector3 = cam.global_basis.z
-		var playerPos : Vector3 = owner.global_position
+		var camForward : Vector3 = -cam.global_basis.z
+		var camPos : Vector3 = cam.global_position
+		var targetDirection = camPos.direction_to(targetPos)
+		# Create a raycast to check if the pathway is clear
+		# to the grappling point
+		var ray : RayCast3D = RayCast3D.new()
+		ray.target_position = targetPos - camPos
+		add_child(ray)
+		ray.force_raycast_update()
+		remove_child(ray)
+		# Checks if clear before continuing
+		if (!ray.is_colliding()):
 		# Calculating the dot product from the forward vector of the camera
 		# to the direction from player to target.
 		# This dot product will be 1.0 if the player is looking directly
 		# at the target.
-		var dot := camForward.dot(targetPos.direction_to(playerPos))
-		print(target.name, ": ", dot)
-		if dot > maxDot:
-			maxDot = dot
-			bestTarget = target
+			var dot := camForward.dot(targetDirection)
+			if dot > maxDot:
+				maxDot = dot
+				bestTarget = target
 	return bestTarget
