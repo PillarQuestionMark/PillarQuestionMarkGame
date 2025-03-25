357f0e501a1ae2edb9f9b8bef28a6def6d9a68ef
Tsanzevino <zeldafanta@gmail.com>
2025-03-25 17:02:03 -0400

    Modified to show currently targetted grapple point

--- Components/grappleable_point.gd
+++ Components/grappleable_point.gd
@@ -2,18 +2,17 @@ class_name GrappleablePoint extends Area3D
 
 @export var inactiveMat : StandardMaterial3D
 @export var activeMat : StandardMaterial3D
+@export var targettedMat : StandardMaterial3D
 
 func _ready():
 	print(get_parent().get_class())
 	assert(get_parent() is GeometryInstance3D)
 
-var grappleActive : bool = false :
-	set(value):
-		grappleActive = value
-		if grappleActive:
-			get_parent().material_override = activeMat
-		else:
-			get_parent().material_override = inactiveMat
+func setActive():
+	get_parent().material_override = activeMat
 
-func toggle():
-	grappleActive = not grappleActive
+func setInactive():
+	get_parent().material_override = inactiveMat
+
+func setTargetted():
+	get_parent().material_override = targettedMat
--- Entities/Player/range_finder.gd
+++ Entities/Player/range_finder.gd
@@ -2,26 +2,37 @@ extends Area3D
 
 var targets : Array[GrappleablePoint] = []
 var cam : SpringArm3D
+var bestTarget : GrappleablePoint
 
 func _ready():
 	cam = owner.Camera
+
+func _physics_process(_delta : float):
+	var newBestTarget : GrappleablePoint = get_best_target()
+	if bestTarget == newBestTarget:
+		return
+	if bestTarget != null:
+		bestTarget.setActive()
+	if newBestTarget != null:
+		newBestTarget.setTargetted()
+	bestTarget = newBestTarget
 
 func _on_area_entered(area):
 	#Toggle color/indicator for grappleable objects
 	if area is not GrappleablePoint:
 		return
-	area.toggle()
+	area.setActive()
 	targets.append(area)
 
 func _on_area_exited(area):
 	#Toggle color/indicator for grappleable objects
 	if area is not GrappleablePoint:
 		return
-	area.toggle()
+	area.setInactive()
 	targets.remove_at(targets.find(area))
 
 func get_best_target() -> GrappleablePoint:
-	var maxDot : float = 0.9
+	var maxDot : float = 0.99
 	var bestTarget = null
 	for target : GrappleablePoint in targets:
 		# Creating Variables for readability.
@@ -29,21 +40,21 @@ func get_best_target() -> GrappleablePoint:
 		var camForward : Vector3 = -cam.global_basis.z
 		var camPos : Vector3 = cam.global_position
 		var targetDirection = camPos.direction_to(targetPos)
-		# Create a raycast to check if the pathway is clear
-		# to the grappling point
-		var ray : RayCast3D = RayCast3D.new()
-		ray.target_position = targetPos - camPos
-		add_child(ray)
-		ray.force_raycast_update()
-		remove_child(ray)
-		# Checks if clear before continuing
-		if (!ray.is_colliding()):
-		# Calculating the dot product from the forward vector of the camera
-		# to the direction from player to target.
-		# This dot product will be 1.0 if the player is looking directly
-		# at the target.
-			var dot := camForward.dot(targetDirection)
-			if dot > maxDot:
+		var dot := camForward.dot(targetDirection)
+		if dot > maxDot:
+			# Create a raycast to check if the pathway is clear
+			# to the grappling point
+			var ray : RayCast3D = RayCast3D.new()
+			ray.target_position = targetPos - camPos
+			add_child(ray)
+			ray.force_raycast_update()
+			remove_child(ray)
+			# Checks if clear before continuing
+			if (!ray.is_colliding()):
+			# Calculating the dot product from the forward vector of the camera
+			# to the direction from player to target.
+			# This dot product will be 1.0 if the player is looking directly
+			# at the target.
 				maxDot = dot
 				bestTarget = target
 	return bestTarget
