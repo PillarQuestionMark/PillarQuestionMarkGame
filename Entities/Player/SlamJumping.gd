9457557f805839e74ba37d4fae55fe0f0296df27
dgoorskey <dersu.goorskey@gmail.com>
2025-03-31 15:49:09 -0400

    fix slamjump audio crash

--- Entities/Player/SlamJumping.gd
+++ Entities/Player/SlamJumping.gd
@@ -16,7 +16,7 @@ func physics_update(_delta: float) -> void:
 ## Called by the state machine upon changing the active state. The `data` parameter
 ## is a dictionary with arbitrary data the state can use to initialize itself.
 func enter(previous_state_path: String, data := {}) -> void:
-	player.jump_sound.play()
+	AudioManager.play_fx("jump")
 	player.velocity.y = player.Slam_Jump_Impulse
 	finished.emit(FALLING)
 
