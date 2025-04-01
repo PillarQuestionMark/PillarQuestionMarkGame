ba460295223fce34bdd6ba74f27a76ca503cf156
dgoorskey <dersu.goorskey@gmail.com>
2025-04-01 12:56:26 -0400

    add editor warnings for island map icons

--- Scenes/screens/change_island_screen/island_icon.gd
+++ Scenes/screens/change_island_screen/island_icon.gd
@@ -1,11 +1,15 @@
+@tool
 extends StaticBody2D
 class_name IslandIcon
 
 @export var island_id := -999 # some dummy id that no island actually uses
-@export_file("*.tscn") var island_scene: String = ""
+@export_file("*.tscn") var island_scene: String = "":
+	set(value):
+		island_scene = value
+		update_configuration_warnings()
 
 func _ready() -> void:
-	assert(not island_scene.is_empty())
+	update_configuration_warnings()
 	$Interact.visible = false
 
 
@@ -20,3 +24,9 @@ func _on_interactable_2d_body_exited(body: Node2D) -> void:
 
 func _on_interactable_2d_on_interacting() -> void:
 	PlayerData.call_deferred("load_scene", island_scene, 0)
+
+func _get_configuration_warnings() -> PackedStringArray:
+	var warnings = []
+	if island_scene.is_empty():
+		warnings.append("Island scene is empty.")
+	return warnings
