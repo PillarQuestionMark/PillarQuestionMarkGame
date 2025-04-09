d6c14ed045fb0b32f6952d5ba4de9a3b64e91afc
SincerelySeven <115359401+SincerelySeven@users.noreply.github.com>
2025-04-08 20:27:55 -0400

    Added interact button to cutscene
    Pressing interact now also works in cutscenes to advance

--- Scenes/screens/cutscene-screen/cutscene_screen.gd
+++ Scenes/screens/cutscene-screen/cutscene_screen.gd
@@ -43,7 +43,8 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	if Input.is_action_just_pressed("interact"):
+		_on_continue()
 	
 func setup(scene_text : Array[String], scene : String, checkpoint : int = 0) -> void:
 	text = scene_text
