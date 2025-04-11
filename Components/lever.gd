ba6ccbff6844975f6caf6f4988524ee06c60ce5f
SincerelySeven <115359401+SincerelySeven@users.noreply.github.com>
2025-04-10 22:08:23 -0400

    Added lever sounds
    They can turn on and off now

--- Components/lever.gd
+++ Components/lever.gd
@@ -11,12 +11,16 @@ func _on_interactable_on_interacting() -> void:
 	if animator.is_playing():
 		return
 	
+	AudioManager.play_fx("Tick")
+	
 	print("[%s] got interacted" % get_path())
 	
 	if is_on:
 		animator.play("flip_off")
+		AudioManager.play_fx("LeverOff")
 	else:
 		animator.play("flip_on")
+		AudioManager.play_fx("LeverOn")
 	
 	is_on = not is_on
 	
