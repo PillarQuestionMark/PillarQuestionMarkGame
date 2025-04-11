7d2ec91082ed9685e6ddee60edeef018164e5fc0
SincerelySeven <115359401+SincerelySeven@users.noreply.github.com>
2025-04-10 21:25:06 -0400

    mushroom now has the jump sound
    jump sound now has the mushroom

--- Obstacles/bouncy_mushroom.gd
+++ Obstacles/bouncy_mushroom.gd
@@ -53,6 +53,7 @@ func _on_area_3d_body_entered(body: Node3D) -> void:
 		player.touched_ground()
 		player.velocity.y = bounce_height
 	
+	AudioManager.play_fx("jump")
 	#if !slam_only or _check_slam(body): ## if slam only check slam, else do nothing
 		#body.touched_ground() ## reset jumps (for double jumping)
 		#body.velocity.y += _check_height(body) ## give bounce impulse
